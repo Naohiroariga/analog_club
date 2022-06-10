ee8cbf95a6faa26e3650d4658dd478dd30e0dcd1
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-06-10 09:39:16 +0000

    [Add]admin_games

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,5 +1,23 @@
 class Admin::GamesController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
+    if params[:new_date]
+      @games = Game.all.order(created_at: :desc)
+    elsif params[:favorite]
+      favorite = Game.find(Favorite.group(:game_id).order('count(game_id) desc').pluck(:game_id))
+      all = Game.all
+      @games = favorite + (all - favorite)
+    else
+      @games = Game.all
+    end
+    tags = []
+    @games.each do |game|
+      game.tags.each do |tag|
+        tags << tag.tag
+      end
+      @tags = tags.uniq
+    end
   end
 
   def show
