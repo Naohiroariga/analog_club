dfaabd6b0378dc935d7154a0ea389052a8946c67
Naohiroariga <yukiyanagi.bs@gmail.com>
2022-06-13 01:49:37 +0000

    [Fix]サインイン前の処理修正

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,5 +1,5 @@
 class Public::GamesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @game = Game.new
